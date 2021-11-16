571b4f89ffc1ea1a1ad39440a858a9bf076bf10b
Ayoub Alfurjani <a.alfurjani@muljin.com>
2021-11-16 21:24:10 +0200

    truffle setup

---
+++ contracts/FaucetContract.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Faucet {
+    // storage variables (they keep state)
+    uint16 public funds = 1000;
+    int public counter = -10;
+}
