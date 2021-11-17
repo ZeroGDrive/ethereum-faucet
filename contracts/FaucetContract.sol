39113fcd534e5cf6de44ca031c673d8b28063b2c
Ayoub Alfurjani <a.alfurjani@muljin.com>
2021-11-17 22:07:08 +0200

    some changes on the contract

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -2,7 +2,15 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
-    // storage variables (they keep state)
-    uint16 public funds = 1000;
-    int public counter = -10;
+
+    // this is a special function
+    // it's called when you make a transaction and doesn't specify function name to call
+
+    // external functions are part of the contract interface
+    // and can be called from other contracts
+
+    // payable means that it can receive ether into this contract
+
+    receive() external payable {   
+    }
 }
